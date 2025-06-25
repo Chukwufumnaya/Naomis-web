e797a7175474f939211478ff946e2261d39c8808
Chukwufumnaya <naomiokelue2002@gmail.com>
2025-06-25 01:45:14 +0100

    Refactor code structure for improved readability and maintainability

--- projects/weather app/api-key.js
+++
@@ -1,4 +0,0 @@
-export const apiKey = 'ec13494ca5518a3354b3705c76dafda3';
-
-export const googleApikey = 'AIzaSyB4UeWm_X0SokHKWMyPHWlSRiAHh1PSy0Q';
-export const googleSearchEngineId = 'a146d043ffd6f42ed&q';
